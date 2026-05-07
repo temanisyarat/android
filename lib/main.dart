e7e9ecdc5587650e79b10931397a1277d9361d03
Hany Wachidatul Aisyah <hanywachidatul1@gmail.com>
2026-05-07 11:26:29 +0700

    coba

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Teman Isyarat',
+      title: 'Teman Isyarat App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MainPage(),
     );
